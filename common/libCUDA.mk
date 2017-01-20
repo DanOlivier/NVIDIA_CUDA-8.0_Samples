35aa4952866398f21cae61156dd4165142f7b42c
Daniel Olivier <dan_olivier@hotmail.com>
2017-01-20 16:38:34 -0500

    Rationalizing makefiles into common.mk, libNVRTC.mk, and libCUDA.mk

---
+++ common/libCUDA.mk
@@ -0,0 +1,63 @@
+UBUNTU = $(shell lsb_release -i -s 2>/dev/null | grep -i ubuntu)
+
+ifeq ($(TARGET_OS),darwin)
+  ALL_LDFLAGS += -Xcompiler -F/Library/Frameworks -Xlinker -framework -Xlinker CUDA
+else
+  CUDA_SEARCH_PATH ?=
+  ifneq ($(TARGET_ARCH),$(HOST_ARCH))
+    ifeq ($(TARGET_ARCH)-$(TARGET_OS),armv7l-linux)
+      ifneq ($(TARGET_FS),)
+        LIBRARIES += -L$(TARGET_FS)/usr/lib
+        CUDA_SEARCH_PATH += $(TARGET_FS)/usr/lib
+        CUDA_SEARCH_PATH += $(TARGET_FS)/usr/lib/arm-linux-gnueabihf
+      endif
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/armv7-linux-gnueabihf/lib/stubs
+      CUDA_SEARCH_PATH += /usr/arm-linux-gnueabihf/lib
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),aarch64-linux)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-linux/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),armv7l-android)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/armv7-linux-androideabi/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),aarch64-android)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-linux-androideabi/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),armv7l-qnx)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/ARMv7-linux-QNX/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),aarch64-qnx)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-qnx/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),ppc64le-linux)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/ppc64le-linux/lib/stubs
+    endif
+  else
+    ifneq ($(UBUNTU),)
+      CUDA_SEARCH_PATH += /usr/lib
+    else
+      CUDA_SEARCH_PATH += /usr/lib64
+    endif
+
+    ifeq ($(TARGET_ARCH),x86_64)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/lib64/stubs
+    endif
+
+    ifeq ($(TARGET_ARCH),armv7l)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/armv7-linux-gnueabihf/lib/stubs
+      CUDA_SEARCH_PATH += /usr/lib/arm-linux-gnueabihf
+    endif
+
+    ifeq ($(TARGET_ARCH),aarch64)
+      CUDA_SEARCH_PATH += /usr/lib
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-linux/lib/stubs
+    endif
+
+    ifeq ($(TARGET_ARCH),ppc64le)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/ppc64le-linux/lib/stubs
+      CUDA_SEARCH_PATH += /usr/lib/powerpc64le-linux-gnu
+    endif
+  endif
+
+  CUDALIB ?= $(shell find -L $(CUDA_SEARCH_PATH) -maxdepth 1 -name libcuda.so)
+  ifeq ("$(CUDALIB)","")
+    $(info >>> WARNING - libcuda.so not found, CUDA Driver is not installed.  Please re-install the driver. <<<)
+    SAMPLE_ENABLED := 0
+  endif
+
+  LIBRARIES += -lcuda
+endif
---
+++ common/libNVRTC.mk
@@ -0,0 +1,74 @@
+# libNVRTC specific libraries
+ifeq ($(TARGET_OS),darwin)
+ LDFLAGS += -L$(CUDA_PATH)/lib -framework CUDA
+else ifeq ($(TARGET_ARCH),x86_64)
+ LDFLAGS += -L$(CUDA_PATH)/lib64 -L$(CUDA_PATH)/lib64/stubs
+else ifeq ($(TARGET_ARCH),ppc64le)
+ LDFLAGS += -L$(CUDA_PATH)/targets/ppc64le-linux/lib -L$(CUDA_PATH)/targets/ppc64le-linux/lib/stubs
+endif
+
+ifeq ($(TARGET_OS),darwin)
+  ALL_LDFLAGS += -Xcompiler -F/Library/Frameworks -Xlinker -framework -Xlinker CUDA
+else
+  CUDA_SEARCH_PATH ?=
+  ifneq ($(TARGET_ARCH),$(HOST_ARCH))
+    ifeq ($(TARGET_ARCH)-$(TARGET_OS),armv7l-linux)
+      ifneq ($(TARGET_FS),)
+        LIBRARIES += -L$(TARGET_FS)/usr/lib
+        CUDA_SEARCH_PATH += $(TARGET_FS)/usr/lib
+        CUDA_SEARCH_PATH += $(TARGET_FS)/usr/lib/arm-linux-gnueabihf
+      endif
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/armv7-linux-gnueabihf/lib/stubs
+      CUDA_SEARCH_PATH += /usr/arm-linux-gnueabihf/lib
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),aarch64-linux)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-linux/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),armv7l-android)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/armv7-linux-androideabi/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),aarch64-android)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-linux-androideabi/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),armv7l-qnx)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/ARMv7-linux-QNX/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),aarch64-qnx)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-qnx/lib/stubs
+    else ifeq ($(TARGET_ARCH)-$(TARGET_OS),ppc64le-linux)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/ppc64le-linux/lib/stubs
+    endif
+  else
+    ifneq ($(UBUNTU),)
+      CUDA_SEARCH_PATH += /usr/lib
+    else
+      CUDA_SEARCH_PATH += /usr/lib64
+    endif
+
+    ifeq ($(TARGET_ARCH),x86_64)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/lib64/stubs
+    endif
+
+    ifeq ($(TARGET_ARCH),armv7l)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/armv7-linux-gnueabihf/lib/stubs
+      CUDA_SEARCH_PATH += /usr/lib/arm-linux-gnueabihf
+    endif
+
+    ifeq ($(TARGET_ARCH),aarch64)
+      CUDA_SEARCH_PATH += /usr/lib
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/aarch64-linux/lib/stubs
+    endif
+
+    ifeq ($(TARGET_ARCH),ppc64le)
+      CUDA_SEARCH_PATH += $(CUDA_PATH)/targets/ppc64le-linux/lib/stubs
+      CUDA_SEARCH_PATH += /usr/lib/powerpc64le-linux-gnu
+    endif
+  endif
+
+  CUDALIB ?= $(shell find -L $(CUDA_SEARCH_PATH) -maxdepth 1 -name libcuda.so)
+  ifeq ("$(CUDALIB)","")
+    $(info >>> WARNING - libcuda.so not found, CUDA Driver is not installed.  Please re-install the driver. <<<)
+    SAMPLE_ENABLED := 0
+  endif
+
+  LIBRARIES += -lcuda
+endif
+
+INCLUDES += -I$(CUDA_PATH)/include
+
+LIBRARIES += -lnvrtc
